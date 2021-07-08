f9426899d0c4c903825b175f92f20c2aac46aedc
Hadi N <hadinurdin99@gamil.com>
2021-07-08 14:50:59 +0700

    1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DefaultModule } from './layouts/default/default.module';
+// import { PostsComponent } from './modules/posts/posts.component';
+// import { HeaderComponent } from './shared/components/header/header.component';
+// import { FooterComponent } from './shared/components/footer/footer.component';
+// import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+// import { DefaultComponent } from './layouts/default/default.component';
+// import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // PostsComponent,
+    // HeaderComponent,
+    // FooterComponent,
+    // SidebarComponent,
+    // DefaultComponent,
+    // DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DefaultModule
   ],
   providers: [],
   bootstrap: [AppComponent]
